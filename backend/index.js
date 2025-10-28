96c748bf0435b3d1e285929097749b275904c5f9
flexteck <flexteckse@gmail.com>
2025-10-28 12:09:37 +0100

    updated some file

--- backend/index.js
+++ backend/index.js
@@ -38,7 +38,7 @@ app.use("/api/category", categoryRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/upload", uploadRoutes);
 app.use("/api/orders", orderRoutes);
-app.use("api/config/paypal", (req, res) => {
+app.use("/api/config/paypal", (req, res) => {
   res.send({ clientId: process.env.PAYPAL_CLIENT_ID });
 });
 
